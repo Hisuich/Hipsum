e389d597afe3cc02dd77ca9debb4bdc71c44e4e8
hisuich <david.barbashov.2000@gmail.com>
2019-01-16 14:16:00 +0200

    remove unnecessary things

--- background.js
+++ background.js
@@ -1,12 +1,1 @@
-<<<<<<< HEAD
-=======
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.contextMenus.create({
-        "id": "Hipsum",
-        "title": "Hipsum",
-        "description": "It's Hipsum"
-    });
-});
-
->>>>>>> 89032eef1db2e04824a69ae7b17bb31237f9bc26
 console.log(chrome);
